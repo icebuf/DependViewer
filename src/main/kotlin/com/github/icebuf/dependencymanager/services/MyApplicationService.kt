0ce4811fb2747c748c8d0ad3a01c21ca07f1590e
GitHub Action <action@github.com>
2021-10-28 09:55:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/icebuf/dependencymanager/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/icebuf/dependencymanager/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.icebuf.dependencymanager.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.icebuf.dependencymanager.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/icebuf/dependencymanager/services/MyProjectService.kt
+++ src/main/kotlin/com/github/icebuf/dependencymanager/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.icebuf.dependencymanager.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.icebuf.dependencymanager.MyBundle
 
 class MyProjectService(project: Project) {
 
